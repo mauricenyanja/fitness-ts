91ce9f51ac038a00b9ccf84b8c43e823c158a8e6
mauricenyanja <mauricee423@gmail.com>
2023-02-01 16:13:12 +0300

    added packages

---
+++ src/scenes/navbar/Link.tsx
@@ -0,0 +1,1 @@
+import { SelectedPage } from '@/shared/types';
--- src/scenes/navbar/index.tsx
+++ src/scenes/navbar/index.tsx
@@ -3,7 +3,7 @@ import { Bars3Icon, XMarkIcon } from "@heroicons/react/24/solid"
 import Logo from "@/assets/Logo.png"
 import Link from "./Link"
 import { SelectedPage } from "@/shared/types"
-import useMediaQuery from "../../hooks/useMediaQuery"
+import useMediaQuery from "@/hooks/useMediaQuery"
 import ActionButton from "@/shared/ActionButton"
 
 
